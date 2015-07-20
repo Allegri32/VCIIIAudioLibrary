6f4b15c23210979dc57bc03ee7d24a1acfca6d34
Allegri32 <allegrirus@gmail.com>
2015-07-20 10:45:47 +0300

    Ported to GTAIII (Alpha 1.1 DevBuild 2)

--- IIIAudioLibrary/IIIAudioLibrary/dllmain.cpp
+++ IIIAudioLibrary/IIIAudioLibrary/dllmain.cpp
@@ -6,6 +6,8 @@
 #define BASS_PLAY_STREAM 	0x7AAA
 #define BASS_LOAD	0x7AAB
 #define BASS_STOP_STREAM 0x7AAC
+#define BASS_PLAY_MOD	0x7ABB
+#define BASS_STOP_MOD	0x7AB1
 
 #define CLEO_VERSION_MAIN    2
 #define CLEO_VERSION_MAJOR   0
@@ -18,8 +20,37 @@ tScriptVar *Params;
 
 int device = -1;
 int freq = 44100;
-int loop;
+int loop = 0;
 HSTREAM streamHandle;
+HMUSIC musicHandle;
+
+eOpcodeResult WINAPI StopMOD(CScript* script)
+{
+	if(BASS_ChannelIsActive(musicHandle) == BASS_ACTIVE_PLAYING) {
+		BASS_ChannelStop(musicHandle);
+		BASS_MusicFree(musicHandle);
+		musicHandle = NULL;
+	}
+	return OR_CONTINUE;
+}
+
+eOpcodeResult WINAPI PlayMOD(CScript* script)
+{
+	script->Collect(1);
+
+	char modpath[100];
+
+	strcpy_s(modpath, Params[0].cVar);
+
+	if(BASS_ChannelIsActive(streamHandle) == BASS_ACTIVE_PLAYING) {
+		BASS_ChannelStop(streamHandle);
+		streamHandle = NULL;
+		loop = 0;
+	}
+		musicHandle = BASS_MusicLoad(FALSE, modpath, 0, 0, BASS_SAMPLE_LOOP, 0);
+		BASS_ChannelPlay(musicHandle, FALSE);
+	return OR_CONTINUE;
+}
 
 eOpcodeResult WINAPI StopStream(CScript* script)
 {
@@ -50,7 +81,7 @@ eOpcodeResult WINAPI PlayStream(CScript* script)
 	BASS_ChannelPlay(streamHandle, FALSE);
 
 	if(loop == 1) {
-		BASS_ChannelFlags(streamHandle, BASS_SAMPLE_LOOP, 0);
+		BASS_ChannelFlags(streamHandle, BASS_SAMPLE_LOOP, BASS_SAMPLE_LOOP);
 	} 
 
 	return OR_CONTINUE;
@@ -70,6 +101,8 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID lpReserved)
 		Opcodes::RegisterOpcode(BASS_PLAY_STREAM, PlayStream);
 		Opcodes::RegisterOpcode(BASS_LOAD, BassLoader);
 		Opcodes::RegisterOpcode(BASS_STOP_STREAM, StopStream);
+		Opcodes::RegisterOpcode(BASS_PLAY_MOD, PlayMOD);
+		Opcodes::RegisterOpcode(BASS_STOP_MOD, StopMOD);
 	}
 	return TRUE;
 }
